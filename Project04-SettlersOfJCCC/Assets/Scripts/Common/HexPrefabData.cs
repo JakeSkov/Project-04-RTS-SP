100c926df5ca20ee474bee75e9bbad9c7a836d32
CBroskow <cbroskow@stumail.jccc.edu>
2015-10-03 02:39:49 -0500

    Friday night commit
    Started prefabs for hex game pieces

---
+++ Project04-SettlersOfJCCC/Assets/Scripts/Common/HexPrefabData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexPrefabData : MonoBehaviour {
+
+	public string hexName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
