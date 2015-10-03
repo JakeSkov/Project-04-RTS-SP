832418b7c71192925cc0100840dca6f56f11d8b3
Darrick2theend <Darrick2theend@gmail.com>
2015-10-03 14:23:08 -0500

    Scene transitions
    Major scene transitions complete

---
+++ Project04-SettlersOfJCCC/Assets/Scripts/CreateGameMenu/CreateGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CreateGame : MonoBehaviour 
+{
+    void Awake()
+    {
+        GameObject.Find("btn_Continue").GetComponent<Button>().enabled = false;
+    }
+
+    public void SelectedLevel()
+    {
+        GameObject.Find("btn_Continue").GetComponent<Button>().enabled = true;
+    }
+
+    public void BackToMain()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void InformationMenu()
+    {
+        CreateDataScript.ShowWindow();
+    }
+}
