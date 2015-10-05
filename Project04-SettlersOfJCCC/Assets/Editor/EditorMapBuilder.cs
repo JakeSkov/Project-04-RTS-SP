b98a736c787627d618f4cf261b028024ae6687b7
JakeSkov <JakeSkov95@gmail.com>
2015-10-04 19:20:24 -0500

    Created Test Scene + Custom Assets
    Also started on my tool

---
+++ Project04-SettlersOfJCCC/Assets/Editor/EditorMapBuilder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public enum TileTypes
+{
+    CLAY,
+    WHEAT,
+    LUMBER,
+    SHEEP
+}
+
+public class EditorMapBuilder : EditorWindow
+{
+    public TileTypes[] tiles = new TileTypes[19];
+
+    public static string levelName;
+    public static string authorName;
+
+    [MenuItem("Editor Tools/Map Creator")]
+
+    static void Init()
+    {
+        EditorMapBuilder mapCreator = (EditorMapBuilder)EditorWindow.GetWindow(typeof(EditorMapBuilder));
+        mapCreator.Show();
+    }
+
+    static void OnGui()
+    {
+        EditorGUILayout.LabelField("Map Builder");
+        EditorGUILayout.Separator();
+
+        EditorGUILayout.BeginVertical();
+        levelName = EditorGUILayout.TextField("Level Name:\t", levelName);
+        authorName = EditorGUILayout.TextField("Created by:\t", authorName);
+        EditorGUILayout.EndVertical();
+    }
+}
