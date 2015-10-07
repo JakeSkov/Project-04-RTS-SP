e3b1fe8d8617d4b3d37dc0a39197da1b8eec9576
JakeSkov <JakeSkov95@gmail.com>
2015-10-07 13:11:49 -0500

    Player UI Done

---
+++ Project04-SettlersOfJCCC/Assets/Scripts/Common/PlayerUIController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// @author Jake Skov
+/// @desc 
+/// </summary>
+public class PlayerUIController : MonoBehaviour 
+{
+    public Text woodAmount;
+    public Text brickAmount;
+    public Text grainAmount;
+    public Text sheepAmount;
+
+    public int wood, brick, grain, sheep;
+
+    public Button buildSettlement;
+    public Button buildRoad;
+
+    void Awake()
+    {
+        //Sets the buttons to noninteractable
+        buildSettlement.interactable = false;
+        buildRoad.interactable = false;
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        UpdateUI();
+        TestBuildables();
+	}
+
+    //Displays the players resources 
+    void UpdateUI()
+    {
+        woodAmount.text = "Wood:\t" + wood;
+        brickAmount.text = "Bricks:\t" + brick;
+        grainAmount.text = "Grain:\t" + grain;
+        sheepAmount.text = "Sheep:\t" + sheep;
+    }
+
+    void TestBuildables()
+    {
+        if (wood > 0 && brick > 0)
+        {
+            buildRoad.interactable = true;
+        }
+        else
+        {
+            buildRoad.interactable = false;
+        }
+
+        if (wood > 0 && brick > 0 && grain > 0 && sheep > 0)
+        {
+            buildSettlement.interactable = true;
+        }
+        else
+        {
+            buildSettlement.interactable = false;
+        }
+    }
+
+    public void SettlementOnClick()
+    {
+        Debug.Log("Built Settlement");
+        wood--;
+        brick--;
+        grain--;
+        sheep--;
+    }
+
+    public void RoadOnClick()
+    {
+        Debug.Log("Built Road");
+        wood--;
+        brick--;
+    }
+}
